069e257711d3667f787e8b486752a8fd993af75d
Kwame Appiah <quarmyappiah@live.com>
2023-09-10 17:29:30 -0700

    simple 0...9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - number to 10 with onlu putchar
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+}
+
+putchar('\n');
+return (0);
+
+}
