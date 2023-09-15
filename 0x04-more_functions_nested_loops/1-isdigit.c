02d53dd2d5be19c4da0a157ce060327e538dbab1
Kwame Appiah <quarmyappiah@live.com>
2023-09-15 15:56:03 -0700

    checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,30 +1,17 @@
 #include "main.h"
-/**
- * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
- */
-
-int mul(int a, int b)
-{
-	return (a * b);
-}
-
-
-2-main.c
-#include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to be checked
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int main(void)
+
+int _isdigit(int c)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
 
