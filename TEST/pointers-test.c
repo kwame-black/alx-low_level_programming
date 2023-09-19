b9757a6e551ce71e9c956d5e533df5f9769021af
Kwame Appiah <quarmyappiah@live.com>
2023-09-19 16:02:04 -0700

    pointing and changing value

---
+++ TEST/pointers-test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Testing pointers and arrays
+ *
+ * Result: Always (0)
+ */
+
+int main (void)
+{
+ int n;
+ 
+ printf("Size of char %lu\n", sizeof(char));
+ printf("size of int %lu\n", sizeof(int));
+
+return (0);
+
+}
