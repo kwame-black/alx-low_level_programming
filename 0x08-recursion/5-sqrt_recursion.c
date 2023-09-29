1d52e4f4b05c038f902f1a9b8ef00638f52eff0d
Kwame Appiah <quarmyappiah@live.com>
2023-09-29 16:29:41 -0700

    the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,4 +26,8 @@ int _sqrt_recursion(int n)
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
-
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
